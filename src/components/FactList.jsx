f0e4e54e5c0aeb9c30a4c98c77485df0394f7fc8
HidayahJadaan <hkaljadaan18@cit.just.edu.jo>
2024-01-16 00:10:13 +0300

    Updating Component Folder

---
+++ src/components/FactList.jsx
@@ -0,0 +1,22 @@
+import Fact from "./Fact";
+
+export default function FactList({ facts, setFacts, CATEGORIES }) {
+    if (facts.length === 0)
+      return (
+        <p className='message'>
+          No facts for this category yet! Create the first one ✌️
+        </p>
+      );
+  
+    return (
+      <section>
+        <ul className='facts-list'>
+          {facts.map((fact) => (
+            <Fact CATEGORIES={CATEGORIES} key={fact.id} fact={fact} setFacts={setFacts} />
+          ))}
+        </ul>
+        <p>There are {facts.length} facts in the database. Add your own!</p>
+      </section>
+    );
+  }
+  
