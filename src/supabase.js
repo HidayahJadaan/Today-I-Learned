9d9451ca5aea91e17d7716a1708c580780ac45ec
HEDAIA KHALIL ALJADAAN <hkaljadaan18@cit.just.edu.jo>
2024-07-04 20:16:29 +0300

    Updating The Database

--- src/supabase.js
+++ src/supabase.js
@@ -1,8 +1,8 @@
-import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://aigvrzfbwquobswctixl.supabase.co";
-const supabaseKey =
-"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFpZ3ZyemZid3F1b2Jzd2N0aXhsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDUzMzUwMzMsImV4cCI6MjAyMDkxMTAzM30.qAQxTboiA5MnAhjVKbbIpji6UxqcMy8I00JtNNGIuBk"
-const supabase = createClient(supabaseUrl, supabaseKey);
+import { createClient } from '@supabase/supabase-js'
+
+const supabaseUrl = 'https://aigvrzfbwquobswctixl.supabase.co'
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFpZ3ZyemZid3F1b2Jzd2N0aXhsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDUzMzUwMzMsImV4cCI6MjAyMDkxMTAzM30.qAQxTboiA5MnAhjVKbbIpji6UxqcMy8I00JtNNGIuBk"
+const supabase = createClient(supabaseUrl, supabaseKey)
 
 export default supabase;
